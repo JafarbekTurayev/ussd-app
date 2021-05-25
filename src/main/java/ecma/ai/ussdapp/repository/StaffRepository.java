90025a587c2742a7b983207849a0e1769057bbbf
Shohruh_Berdikulov <berdikulovshohruh2002@gmail.com>
2021-05-25 21:50:44 +0500

    Staff_Shohruh

---
+++ src/main/java/ecma/ai/ussdapp/repository/StaffRepository.java
@@ -0,0 +1,7 @@
+package ecma.ai.ussdapp.repository;
+
+import ecma.ai.ussdapp.entity.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaffRepository extends JpaRepository<Staff,Integer> {
+}
